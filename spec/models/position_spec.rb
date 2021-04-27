afc844a72af073e28408477acbb80607c5baff7c
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-27 16:17:15 +0200

    spec code refactoring

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -2,18 +2,19 @@
 RSpec.describe Position, type: :model do
   describe "db table " do
     it{is_expected.to have_db_column :title}
+  end  
+    
+  describe "db table " do
+    it{is_expected.to have_db_column(:description).of_type(:string)}
   end
+
+  describe "db table " do
+    it{is_expected.to have_db_column(:price).of_type(:integer) }
+  end
+
   describe "factory" do
     it'expected to have valid factory' do
       expect(create(:position)).to be_valid
     end
-    
-    describe "db table " do
-      it{is_expected.to have_db_column(:description).of_type(:string)}
-    end
-    describe "db table " do
-      it{is_expected.to have_db_column(:price).of_type(:integer) }
-    end
-
   end
 end
--- spec/requests/api/api_responds_collection_of_menu_items_spec.rb
+++ spec/requests/api/api_responds_collection_of_menu_items_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe 'GET /api/positions/', type: :request do
 
-let!(:position_1) {create(:position, title: 'kebab', description: 'rice with kebab', price: 100)}
+let!(:position_1) {create(:position, 
+  title: 'kebab', 
+  description: 'rice with kebab', 
+  price: 100
+)}
 let!(:position_2) {create(:position)}
 
   describe('successfully') do
@@ -12,8 +16,12 @@ let!(:position_2) {create(:position)}
       expect(response).to have_http_status 200
     end
 
-    it 'is expected to have a title' do 
+    it 'is expected to have a 2 positions on the menu' do 
       expect(response_json['positions'].count).to eq 2
+    end
+
+    it 'is expected position to have a title' do 
+      expect(response_json['positions'].first['title']).to eq 'kebab'
     end
   end
 end 
