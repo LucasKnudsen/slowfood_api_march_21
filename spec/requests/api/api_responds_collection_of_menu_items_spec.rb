d6afb73a2d0cb6d728c07593def03f3a63786d0e
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-28 12:59:43 +0200

    fixes typo in route in spec file

--- spec/requests/api/api_responds_collection_of_menu_items_spec.rb
+++ spec/requests/api/api_responds_collection_of_menu_items_spec.rb
@@ -1,10 +1,10 @@
-RSpec.describe 'GET /api/menuitems/', type: :request do
+RSpec.describe 'GET /api/menu_items/', type: :request do
   let!(:menu_item_1) { create(:menu_item, title: 'kebab', description: 'rice with kebab', price: 100) }
   let!(:menu_item_2) { create(:menu_item) }
 
   describe('successfully') do
     before do
-      get '/api/menuitems'
+      get '/api/menu_items'
     end
 
     it 'is expected to respond with status 200' do
