56d49c0661ad56ba6e2c207d309ac32b1ec967a2
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-28 10:19:11 +0200

    Revert "fixes typo in model spec"
    This reverts commit 3de07de6e0e3218aadeb156ff1e279c75c379e83.

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Position, type: :model do
     it { is_expected.to have_db_column(:price).of_type(:integer) }
   end
 
-  describe 'Validations' do
+  describe 'Validation of title' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :price }
   end
