3de07de6e0e3218aadeb156ff1e279c75c379e83
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-28 10:14:06 +0200

    fixes typo in model spec

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Position, type: :model do
     it { is_expected.to have_db_column(:price).of_type(:integer) }
   end
 
-  describe 'Validation of title' do
+  describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :price }
   end
