e6b10c60e2b36b6a80dab9c07f755090e30d5f21
kim_haaga <kim.haaga@live.se>
2021-04-29 21:01:28 +0200

    Add test for wrong pw confirmation

--- spec/requests/api/user_can_register_account_spec.rb
+++ spec/requests/api/user_can_register_account_spec.rb
@@ -19,4 +19,23 @@ RSpec.describe 'POST /api/auth', type: :request do
       expect(response_json['data']['email']).to eq 'user@email.com'
     end
   end
+
+  describe 'types wrong password confirmation' do
+    before do 
+      post '/api/auth',
+        params: {
+          email: 'user@email.com',
+          password: 'password',
+          password_confirmation: 'wrong_pw'
+        }
+    end 
+
+    it 'is expected to respond with status 422' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'returns expected error message' do
+      expect(response_json['errors']['password_confirmation'].first).to eq "doesn't match Password"
+    end
+  end
 end
