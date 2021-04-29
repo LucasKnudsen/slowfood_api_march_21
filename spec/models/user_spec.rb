df5e26323fd61cc2df4ecef690b0f81c0ba076a7
luke <lukeperrin27@gmail.com>
2021-04-29 13:30:07 +0200

    adds validtions to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'db table' do
+    it { is_expected.to have_db_column(:email).of_type('string') }
+    it { is_expected.to have_db_column(:password).of_type('string') }
+  end
+
+  describe "has validators" do
+    it {is_expected.to validate_presence_of :email}
+    it {is_expected.to validate_presence_of :password}
+    
+  end
+  
 end
