ec14a01d9585dc6b5f410d239c8769ab828d2925
luke <lukeperrin27@gmail.com>
2021-04-30 10:27:27 +0200

    removes extra validation of email and password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
-  validates_presence_of :email, :encrypted_password
 end
